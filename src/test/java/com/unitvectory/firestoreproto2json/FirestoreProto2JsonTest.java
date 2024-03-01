3d4d5ae2ee8c18715c4e35363bf8150de1b4a23c
Jared Hatfield <jjhatf02@gmail.com>
2024-02-29 22:27:57 -0500

    Added missing comments to test files

--- src/test/java/com/unitvectory/firestoreproto2json/FirestoreProto2JsonTest.java
+++ src/test/java/com/unitvectory/firestoreproto2json/FirestoreProto2JsonTest.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.unitvectory.firestoreproto2json;
 
 import java.util.Base64;
@@ -5,6 +18,11 @@ import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import com.google.events.cloud.firestore.v1.DocumentEventData;
 
+/**
+ * FirestoreProto2Json tests for the builder
+ * 
+ * @author Jared Hatfield (UnitVectorY Labs)
+ */
 public class FirestoreProto2JsonTest {
 
         private static final String booleanUpdate =
